9bcc50ff3bfb426bf8186da36db1d8a71057af7f
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-07-13 12:33:27 +0200

    Fix build.sh script

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ set -o pipefail
 IFS=$'\n\t'
 SCRIPT_FOLDER="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-#"${SCRIPT_FOLDER}/memberOrganizationsBenefits.sh"
+"${SCRIPT_FOLDER}/memberOrganizationsBenefits.sh"
 jsonnet "${SCRIPT_FOLDER}/cbiSponsorships.jsonnet" > "${SCRIPT_FOLDER}/cbiSponsorships.json"
